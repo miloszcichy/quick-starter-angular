37ca904b8e55e2da93b81567d67613ed6220d89d
A687991 <milosz.cichy@atos.net>
2023-02-08 14:28:15 +0100

    add html2pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { html2pdf } from 'html2pdf.js'
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private onDestroy$: Subject<boolean> = new Subject<boolean>();
+    private onDestroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() {}
+    constructor() {
+    }
 
-  ngOnDestroy(): void {
-    this.onDestroy$.next(true);
-    this.onDestroy$.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        this.onDestroy$.next(true);
+        this.onDestroy$.unsubscribe();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        const element = document.getElementById("test");
+        html2pdf(element);
+    }
 }
